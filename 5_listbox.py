99aecb92ff9523a96aa8b158ecaf049ca0af8417
hyunsu3408 <hyunsu3408@gmail.com>
2025-06-07 22:13:45 +0900

    a

--- 5_listbox.py
+++ 5_listbox.py
@@ -14,7 +14,17 @@ listbox.pack()
 
 
 def btncmd():
-    pass
+    listbox.delete(END) #맨 뒤 항목을 삭제
+    # listbox.delete(0) # 맨 앞부터 삭제
+
+    # 갯수
+    print("리스트에는",listbox.size(),"개가 있어요")
+    
+    # 항목 확인 (시작 idx, 끝 idx)
+    print("1번째 부터 3번째까지의 항목: ",listbox.get(0,2))
+
+    # 선택된 항목 확인 (위치로 반환 ex(1,2,3))
+    print("선택된 항목: ", listbox.curselection())
 
 btn = Button(root,text="클릭",command=btncmd)
 btn.pack()
