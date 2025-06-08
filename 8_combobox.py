3bd1735068502982b148ee30555e5328abfd7c43
hyunsu3408 <hyunsu3408@gmail.com>
2025-06-08 23:17:48 +0900

    A

---
+++ 8_combobox.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import tkinter.ttk as ttk
+
+root = Tk()
+root.title("hyunsu GUI")
+root.geometry("640x480+350+200")
+
+values = [str(i)+"일" for i in range(1,32)]
+combobox= ttk.Combobox(root, height=5, values=values)
+combobox.pack()
+combobox.set("카드 결제일") #최초 목록 설정
+
+readonly_combobox= ttk.Combobox(root, height=10, values=values, state="readonly")
+readonly_combobox.current(0)
+readonly_combobox.pack()
+
+
+
+def btncmd():
+    print(combobox.get())
+    print(readonly_combobox.get())
+
+btn = Button(root,text="선택",command=btncmd)
+btn.pack()
+
+
+
+root.mainloop()
+
+
+
+
+
